797206a9d9ae0fe974add81ea16a60fcf22e5ff0
valeriinikolaichuk <powerlift.rv@gmail.com>
2026-06-19 15:17:11 +0200

    swagger

--- auth-api/src/main/java/com/winwin/auth_api/AuthPathApplication.java
+++ auth-api/src/main/java/com/winwin/auth_api/AuthPathApplication.java
@@ -1,4 +1,4 @@
-package com.winwin.auth_path;
+package com.winwin.auth_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ auth-api/src/main/java/com/winwin/auth_api/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.winwin.auth_api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "WinWin Auth API",
+                version = "1.0",
+                description = "Authentication and processing service"
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+    
+}
